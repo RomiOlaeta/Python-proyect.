6147f1ca65403cd32d0449be0852488ee0292654
Romina Olaeta <113724968+RomiOlaeta@users.noreply.github.com>
2023-08-14 20:26:58 -0300

    Add files via upload

---
+++ Base_UnitTest.py
@@ -0,0 +1,29 @@
+import time
+import driver as driver
+import unittest
+from idlelib import search
+from selenium import webdriver
+from selenium.webdriver.chrome import options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import keys, Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+
+    def test1(self):
+        driver.get("https://demoqa.com/text-box")
+        time.sleep(4)
+    def tearDown(self):
+        driver.close()
+
+if __name__=='__main__':
+<<<<<<< HEAD
+ unittest.main()
+=======
+ unittest.main()
+>>>>>>> origin/master1
